dd2ceac582d28ae6b9c752f41d0fceb06f498bc5
Toe Naing Lin <2576694n@student.gla.ac.uk>
2024-03-15 17:37:58 +0000

    Added Songs page and Songs Review page

--- symphonet/symphonet1/views.py
+++ symphonet/symphonet1/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from symphonet1.models import Song, Artist, Album
-from symphonet1.forms import UserForm, UserProfileForm
+from django.http import HttpResponse, JsonResponse
+from symphonet1.models import Song, Artist, Album, Rating, Playlist
+from symphonet1.forms import UserForm, UserProfileForm, PlaylistForm
 from django.contrib.auth import authenticate, login,logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.template import loader
+
+SONGS_IN_PAGE = 5
 
 def index(request):
     top_artists = Artist.objects.order_by('-avgRating')[:5]
@@ -28,6 +33,41 @@ def user_reviews(request):
 def user_playlists(request):
     return render(request, 'symphonet/playlists.html')
 
+def song_review(request, songid):
+    song = Song.objects.get(id=songid)
+    rating = Rating.objects.all().filter(song_id=songid)
+    context_dict = {}
+    context_dict['song'] = song
+    try:
+        context_dict['rating'] = rating
+    except Rating.DoesNotExist:
+        context_dict['rating'] = None
+        
+    return render(request, 'symphonet/song_review.html', context = context_dict)
+
+class SongListView(ListView):
+    paginate_by = SONGS_IN_PAGE
+    model = Song
+    template_name = 'symphonet/songs.html'
+    
+def songs(request,page):
+    all_songs = Song.objects.all().order_by('name')
+    paginator = Paginator(all_songs, per_page=SONGS_IN_PAGE)
+    
+    page_object = paginator.get_page(page)
+    context_dict = {'page_obj': page_object}
+    return render(request,'symphonet/songs.html', context = context_dict)
+
+@login_required
+def playlist(request):
+    all_playlists = Playlist.objects.all()
+    context_dict = {}
+    try:
+        context_dict['playlists'] = all_playlists
+    except Playlist.DoesNotExist:
+        context_dict['playlists'] = None
+    return render(request, 'symphonet/playlists.html', context = context_dict)
+    
 def sign_up(request):
     registered = False
     if request.method == 'POST':
